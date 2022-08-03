7a0b2cb532e3b83a8d50d145fd65b8d5bcb8b876
Giovanni Rosa <giovanni.rosa@unimol.it>
2022-08-03 17:34:13 +0200

    fix

--- tool/run_pipeline.sh
+++ tool/run_pipeline.sh
@@ -12,7 +12,7 @@ PHASE2_DIR="2-phase-2-dockerfile-asts"
 PHASE3_DIR="3-phase-3-dockerfile-asts"
 PHASE4_DIR="4-abstracted-asts"
 
-ASTS_OUT_DIR="${DIR}/${PHASE4}/outputs"
+ASTS_OUT_DIR="${DIR}/${PHASE4_DIR}/outputs"
 
 # Check if docker is running
 if ! docker info > /dev/null 2>&1; then
